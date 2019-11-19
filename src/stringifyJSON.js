3685deca2f5a16129d732a6ff51c4c75c9803d3e
Abdlrrahman <56532938+Abdlrrahman@users.noreply.github.com>
2019-11-20 00:16:13 +0200

    1

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,13 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
+/ this is what you would do if you liked things to be easy:
 // but you don't so you're going to write it from scratch:
-
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function(obj, i, str) {
+    var i = i || 0;
+        if(i === Object.keys(obj).length) {
+           return '{' + str.slice(str.length - 1) + '}';
+   }
+   var keys = Object.keys(obj)[i];
+   var values = Object.values(obj)[i];
+   var str = str || '';
+   str += '"' + keys + '"' + ':'+ values + ',';
+   return stringifyJSON(obj, i + 1, str)
 };
